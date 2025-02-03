c6b2772f10ffc19d4e157932a716d456a832714d
tiffany <sulatttweltarbo16@gmail.com>
2025-02-03 13:54:55 +0800

    first commit

---
+++ src/test/java/com/cleaning_service/ws/WsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleaning_service.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
